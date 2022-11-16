556bb7c989c969130285f294a8d623f759a085a1
Edgar Vardanyan <e.vardan4an@gmail.com>
2022-11-17 01:28:44 +0400

    solved

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -14,26 +14,32 @@ import java.io.IOException;
 
 @WebServlet("/add")
 public class AddUserServlet extends HttpServlet {
-    //write your code here!
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher view = req.getRequestDispatcher("jsp/add.jsp");
-        view.forward(req, resp);
-
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("jsp/add.jsp");
+        try {
+            requestDispatcher.forward(req, resp);
+        } catch (ServletException | IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String firstName = req.getParameter("firstName");
         String lastName = req.getParameter("lastName");
-
         User user = new User(firstName, lastName);
-        Warehouse.getInstance().addUser(user);
+        Warehouse warehouse = Warehouse.getInstance();
+        warehouse.addUser(user);
 
-        req.setAttribute("user", user);
-        resp.sendRedirect("/add");
+        req.setAttribute("userName", firstName);
+        req.setAttribute("lastName", lastName);
+
+        try {
+            resp.sendRedirect("/add");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
 
 }
--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -19,17 +19,13 @@ public class GetUsersServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-
         Set<User> users = Warehouse.getInstance().getUsers();
-
         req.setAttribute("users", users);
-
-        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/jsp/users.jsp");
-
-        requestDispatcher.forward(req, resp);
-
-        resp.sendRedirect("/users");
-
-
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("jsp/users.jsp");
+        try {
+            requestDispatcher.forward(req, resp);
+        } catch (ServletException | IOException e) {
+            e.printStackTrace();
+        }
     }
 }
